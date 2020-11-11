edf159c6a2d5e743656e4b9817dfbe2ed97318b5
krae <kraewind@gmail.com>
2020-11-11 15:22:04 -0500

    commit tree 7a4e3e97dc77b43046e8a21674f8e1744de20d2f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length1, :length2, :length3
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if (((@length1 + @length2) > @length3) && ((@length1 + @length3) > @length2) && ((@length3 + @length2) > @length1)) && !(@length1 <= 0.0 || @length2 <= 0.0 || @length3 <= 0.0) 
+      if @length1 == @length2 && @length2 == @length3
+        :equilateral
+      elsif @length1 != @length2 && @length1 != @length3 && @length2 != @length3
+        :scalene
+      else
+        :isosceles
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "BEEEEEEEEP"
+    end
+  end
+
+
 end
